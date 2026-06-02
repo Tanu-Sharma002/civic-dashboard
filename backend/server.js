4ec5be223f4e50d42938a29183c5f7a2ce738c38
Tanu-Sharma002 <ts10121416@gmail.com>
2026-06-02 20:52:44 +0530

    env configure

--- backend/server.js
+++ backend/server.js
@@ -269,7 +269,8 @@ app.get("/api/data", auth, async (req, res) => {
   }
 
 });
+const PORT = process.env.PORT || 5050;
 
-app.listen(5050, () => {
-  console.log("Server running on port 5050");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
