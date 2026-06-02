fc772226639a4c9f7bb6ded33061acd3744ab124
Tanu-Sharma002 <ts10121416@gmail.com>
2026-06-02 20:32:02 +0530

    env configure

--- backend/db.js
+++ backend/db.js
@@ -1,18 +1,19 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Tanu@8171",
-    database: "civic_dashboard"
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
 });
 
 db.connect((err) => {
-    if (err) {
-        console.log("Database Error:", err);
-    } else {
-        console.log("MySQL Connected");
-    }
+  if (err) {
+    console.log("Database Error:", err);
+  } else {
+    console.log("MySQL Connected");
+  }
 });
 
 module.exports = db;
